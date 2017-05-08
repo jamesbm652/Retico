2a21f7b351a16f134d5f3048c798f564e13cc692
jamesbm652 <jamesbm652@gmail.com>
2017-05-08 16:24:32 -0600

    Implemetar el View.OnClickListener y Metodo onClick

--- app/src/main/java/com/proyecto/udata/retico/Login.java
+++ app/src/main/java/com/proyecto/udata/retico/Login.java
@@ -2,12 +2,18 @@ package com.proyecto.udata.retico;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
