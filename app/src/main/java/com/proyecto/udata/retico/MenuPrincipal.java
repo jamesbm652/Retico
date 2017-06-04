f44a7cd4ea8f493880f9793ef4650b5f51a61891
jamesbm652 <jamesbm652@gmail.com>
2017-06-04 08:31:04 -0600

    Estructura de Listar Equipos implementada. Clase ListarEquipos y layout vacios

--- app/src/main/java/com/proyecto/udata/retico/MenuPrincipal.java
+++ app/src/main/java/com/proyecto/udata/retico/MenuPrincipal.java
@@ -8,20 +8,31 @@ import android.widget.Button;
 
 public class MenuPrincipal extends AppCompatActivity implements View.OnClickListener{
 
-    Button btnModificar;
+    Button btnModificar, btnListarEquipos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
 
         btnModificar = (Button)findViewById(R.id.btnModificar);
+        btnListarEquipos = (Button)findViewById(R.id.btnListarEquipos);
 
         btnModificar.setOnClickListener(this);
+        btnListarEquipos.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(getApplicationContext(),ManejoJugadores.class);
-        startActivity(intent);
+        switch (v.getId()){
+            case R.id.btnModificar:
+                Intent ventModificar = new Intent(getApplicationContext(),ManejoJugadores.class);
+                startActivity(ventModificar);
+                break;
+            case R.id.btnListarEquipos:
+                Intent ventListaEquipos = new Intent(getApplicationContext(), ListaEquipos.class);
+                startActivity(ventListaEquipos);
+                break;
+        }
+
     }
 }
