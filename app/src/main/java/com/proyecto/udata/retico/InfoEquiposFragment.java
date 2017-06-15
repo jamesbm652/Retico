731323257b1013620b4ce95b0b40bfcd3dd199e0
jamesbm652 <jamesbm652@gmail.com>
2017-06-14 18:04:45 -0600

    Informacion de equipos FUNCIONANDO! Integrado con la interfaz de Oscar

--- app/src/main/java/com/proyecto/udata/retico/InfoEquiposFragment.java
+++ app/src/main/java/com/proyecto/udata/retico/InfoEquiposFragment.java
@@ -7,14 +7,22 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by James on 4/6/2017.
  */
 
 public class InfoEquiposFragment extends Fragment implements View.OnClickListener{
+    EditText txtNombreEquipo, txtEncargado, txtPromedioEdad, txtLocalizacion, txtContacto;
+    ListView listaJugadores;
     ImageButton btnBackSpaceMostrarEquipos;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -23,7 +31,28 @@ public class InfoEquiposFragment extends Fragment implements View.OnClickListene
         btnBackSpaceMostrarEquipos = (ImageButton)view.findViewById(R.id.btnBackSpaceMostrarEquipos);
         btnBackSpaceMostrarEquipos.setOnClickListener(this);
 
+        inicializarComponentes(view);
+
+        txtNombreEquipo.setText(getArguments().getString("nombreEquipo"));
+        txtEncargado.setText(getArguments().getString("nombreEncargado"));
+        txtPromedioEdad.setText(""+getArguments().getInt("edadPromedio"));
+        txtLocalizacion.setText(getArguments().getString("localizacion"));
+        txtContacto.setText(getArguments().getString("contacto"));
+
+        ArrayList<String> elementosLista = getArguments().getStringArrayList("listaJugadores");
+        ArrayAdapter adaptador = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, elementosLista);
+        listaJugadores.setAdapter(adaptador);
+
         return view;
+    }
+
+    private void inicializarComponentes(View v){
+        txtNombreEquipo = (EditText)v.findViewById(R.id.txtNombreEquipo);
+        txtEncargado = (EditText)v.findViewById(R.id.txtEncargado);
+        txtPromedioEdad = (EditText)v.findViewById(R.id.txtPromedioEdad);
+        txtLocalizacion = (EditText)v.findViewById(R.id.txtLocalizacion);
+        txtContacto = (EditText)v.findViewById(R.id.txtContacto);
+        listaJugadores = (ListView)v.findViewById(R.id.listaJugadores);
     }
 
     @Override
